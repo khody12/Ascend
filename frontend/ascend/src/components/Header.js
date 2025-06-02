3acae7ff34d8be722bd13a6e5873f8f8c67e6a86
khody <ekhodor8@gmail.com>
2025-06-02 14:34:26 -0700

    moving over to tailwind, initial setup n test done

--- frontend/ascend/src/components/Header.js
+++ frontend/ascend/src/components/Header.js
@@ -3,30 +3,53 @@ import "./Header.css"
 import { Link } from "react-router-dom";
 import {AuthContext} from "../AuthContext";
 
-function Header({ username }) {
-    const { authData } = useContext(AuthContext)
+// function Header({ username }) {
+//     const { authData } = useContext(AuthContext)
     
-    return (
-        <header className="app-header">
-            <nav className="nav">
-                <div className="nav-list">
+//     return (
+//         <header className="app-header">
+//             <nav className="nav">
+//                 <div className="nav-list">
 
-                    <Link to="/login">Home</Link>
-                    <Link to="/Dashboard">Dashboard</Link>
-                    {!authData && (
-                    <Link to="/Login">Login</Link>
-                    )}
-                    {authData && (
+//                     <Link to="/login">Home</Link>
+//                     <Link to="/Dashboard">Dashboard</Link>
+//                     {!authData && (
+//                     <Link to="/Login">Login</Link>
+//                     )}
+//                     {authData && (
                         
-                        <Link to="/profile" className="username-link">
-                            {authData.username}
-                        </Link>
+//                         <Link to="/profile" className="username-link">
+//                             {authData.username}
+//                         </Link>
                         
-                    )}
+//                     )}
+//                 </div>
+//             </nav>
+//         </header>
+//     )
+// }
+
+export default function Header({ username }) {
+    const { authData } = useContext(AuthContext)
+
+    return (
+        <header className="app-header bg-green-400"> {/* <-- ADD THIS TEST CLASS */}
+            <nav className="nav">
+            <div className="nav-list">
+                <Link to="/login">Home</Link>
+                <Link to="/Dashboard">Dashboard</Link>
+                {!authData && (
+                <Link to="/Login">Login</Link>
+                )}
+                {authData && (
+                    <Link to="/profile" className="username-link">
+                        {authData.username}
+                    </Link>
+                )}
                 </div>
             </nav>
+            
         </header>
     )
 }
 
-export default Header;
