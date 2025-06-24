9b8c001a051367b42f008944d8ca8153364a8181
khody <ekhodor8@gmail.com>
2025-06-24 12:46:42 -0700

    new secret key so i can make repo public

--- backend/ascend/settings.py
+++ backend/ascend/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zy#rrb7if0&c@j0n@nsyfh@@))a)c#4igu***(a4mw4*9qrm(_'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
